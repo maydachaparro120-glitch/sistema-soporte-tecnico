a035a05677f365ca3364801e8e5bb58f0a7d08ec
maydachaparro120-glitch <maydachaparro120@gmail.com>
2026-05-24 13:22:41 -0500

    Delete frontend directory

--- frontend/Login.jsx
+++
@@ -1,9 +0,0 @@
-function Login() {
-  return (
-    <div>
-      <h2>Inicio de sesión</h2>
-    </div>
-  );
-}
-
-export default Login;
--- frontend/Tickets.jsx
+++
@@ -1,9 +0,0 @@
-function Tickets() {
-  return (
-    <div>
-      <h2>Gestión de Tickets</h2>
-    </div>
-  );
-}
-
-export default Tickets;
